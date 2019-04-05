09d658066c2fce82e39329836a4d90d99e7a4112
guoyangqiao <Gu0yangqiao>
2019-04-05 14:32:12 +0800

    autocommit

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const bot = require('./bot').wechatyBot;
 const botContext = require('./bot').botContext;
 const http = require('http');
-
+const fs = require('fs');
 http.createServer((request, response) => {
     const {headers, method, url} = request;
-    let body = [];
+    let requestBody = [];
     request.on('error', (err) => {
         console.error(err);
     }).on('data', (chunk) => {
-        body.push(chunk);
+        requestBody.push(chunk);
     }).on('end', () => {
-        body = Buffer.concat(body).toString();
-        response.writeHead(200, {'Content-Type': 'application/json'});
+        requestBody = Buffer.concat(requestBody).toString();
         response.on('error', (err) => {
             console.error(err);
         });
-        let respBody = {};
         let area = url.toString().split('/');
         if (area.length > 2 && area[1].toUpperCase() === 'LIFECYCLE') {
+            let respBody = {};
+            response.writeHead(200, {'Content-Type': 'application/json'});
             let event = area[2];
             switch (event.toUpperCase()) {
                 case 'SCAN':
@@ -29,7 +29,11 @@ http.createServer((request, response) => {
                     break;
                 default:
             }
+            response.end(JSON.stringify(respBody));
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            let readStream = fs.createReadStream("index.html", "UTF-8");
+            readStream.pipe(response);
         }
-        response.end(JSON.stringify(respBody));
     });
 }).listen(3000);
