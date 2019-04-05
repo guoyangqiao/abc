e6baea71cda7a000838bacc65d896a9d79c0f3b1
guoyangqiao <Gu0yangqiao>
2019-04-05 14:38:50 +0800

    autocommit

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ http.createServer((request, response) => {
             }
             response.end(JSON.stringify(respBody));
         } else {
+            console.log("index.html");
             response.writeHead(200, {'Content-Type': 'text/html'});
             let readStream = fs.createReadStream("index.html", "UTF-8");
             readStream.pipe(response);
