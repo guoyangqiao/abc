8b1b1145c36b8ffd6d8dd2fcdf620df22cdcb516
guoyangqiao <Gu0yangqiao>
2019-04-05 16:17:58 +0800

    autocommit

---
+++ js/app.js
@@ -0,0 +1,1 @@
+ReactDOM.render('hello', document.getElementById('root'));
--- js/server.js
+++ js/server.js
@@ -31,8 +31,13 @@ http.createServer((request, response) => {
             }
             response.end(JSON.stringify(respBody));
         } else {
+            let readStream;
+            if (url === '/') {
+                readStream = fs.createReadStream('index.html', 'UTF-8');
+            } else {
+                readStream = fs.createReadStream(url.toString().substr(1), 'UTF-8');
+            }
             response.writeHead(200, {'Content-Type': 'text/html'});
-            let readStream = fs.createReadStream("_page/" + "index.html", "UTF-8");
             readStream.pipe(response);
         }
     });
