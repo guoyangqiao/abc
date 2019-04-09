a559fbe1321ac460529b20f7a98b948828ad20a3
guoyangqiao <Gu0yangqiao>
2019-04-09 10:34:59 +0800

    autocommit

--- js/server.js
+++ js/server.js
@@ -2,15 +2,26 @@ const bot = require('./wechaty').wechatyBot;
 const botContext = require('./wechaty').botContext;
 const http = require('http');
 const fs = require('fs');
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 
+const app = express();
 
-function writeResponse(respBody, response) {
-    let chunk = JSON.stringify(respBody);
-    response.end(chunk);
-}
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-function codeContactList(response) {
+app.get('/lifecycle/scan', (req, response) => {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    if (!bot.logonoff()) {
+        response.send(botContext.qrCode);
+    } else {
+        response.send('');
+    }
+});
+app.get('/lifecycle/logon/contact', (req, response) => {
     let aliasPromise = [];
     bot.Contact.findAll().then((clist) => {
         for (let c of clist) {
@@ -21,80 +32,21 @@ function codeContactList(response) {
             }
         }
         Promise.all(aliasPromise).then(result => {
-            writeResponse(result, response);
+            response.status(200).json(result);
         });
     });
-}
+});
 
-let app = express();
+app.put('/lifecycle/logon/message/:logonAction', (req, response) => {
+    let logonAction = req.params.logonAction;
+    if ('FILE' === logonAction.toUpperCase()) {
+        console.log("上传文件todo");
+        fs.writeFileSync('.pom.xml', req.botContext.content, 'UTF-8');
+        response.status(200).end();
+    }
+    if ('words' === logonAction.toUpperCase()) {
+        console.log("发布圣旨");
+    }
+});
+app.listen(3000);
 
-const options = {
-    dotfiles: 'ignore',
-    etag: false,
-    extensions: ['htm', 'html'],
-    index: false,
-    maxAge: '1d',
-};
-
-app.use(express.static('public', options));
-
-http.createServer((request, response) => {
-    const {headers, method, url} = request;
-    let requestBody = [];
-    request.on('error', (err) => {
-        console.error(err);
-    }).on('data', (chunk) => {
-        requestBody.push(chunk);
-    }).on('end', () => {
-        requestBody = Buffer.concat(requestBody).toString();
-        response.on('error', (err) => {
-            console.error(err);
-        });
-        let area = url.toString().split('/');
-        if (area.length > 2 && area[1].toUpperCase() === 'LIFECYCLE') {
-            let respBody = {};
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            let event = area[2];
-            console.log("获取到数据请求,", url);
-            switch (event.toUpperCase()) {
-                case 'SCAN':
-                    if (!bot.logonoff()) {
-                        respBody.qrCode = botContext.qrCode;
-                    } else {
-                        respBody.qrCode = '';
-                    }
-                    writeResponse(respBody, response);
-                    break;
-                case "LOGON":
-                    console.log("获取到登录后的操作", area[3]);
-                    let act = area[3];
-                    if ('CONTACT' === act.toUpperCase()) {
-                        codeContactList(response);
-                    }
-                    if ('FILE' === act.toUpperCase()) {
-                        console.log("上传文件todo");
-                        fs.writeFileSync('.pom.xml', requestBody, 'UTF-8');
-                        writeResponse(respBody, response);
-                    }
-                    break;
-                default:
-
-            }
-        } else {
-            let code = 200;
-            let readStream;
-            if (url === '/') {
-                readStream = fs.readFileSync('index.html', 'UTF-8');
-            } else {
-                let path = url.toString().substr(1);
-                if (fs.existsSync(path)) {
-                    readStream = fs.readFileSync(path, 'UTF-8');
-                } else {
-                    code = 404;
-                }
-            }
-            response.writeHead(code, {'Content-Type': 'text/html'});
-            response.end(readStream);
-        }
-    });
-}).listen(3000);
