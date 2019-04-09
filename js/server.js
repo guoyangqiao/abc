73f24d248a2fbf663c441aadea1ab195854ba0ec
guoyangqiao <Gu0yangqiao>
2019-04-09 20:54:00 +0800

    autocommit

--- js/server.js
+++ js/server.js
@@ -103,7 +103,7 @@ app.listen(3000);
  * @returns {Promise<*>}
  */
 async function snooze() {
-    let sleepPeriod = Math.floor(Math.random() * Math.floor(1000)) + 200;
+    let sleepPeriod = Math.floor(Math.random() * Math.floor(1000)) + 20000;
     return new Promise(resolve => setTimeout(resolve, sleepPeriod));
 }
 
