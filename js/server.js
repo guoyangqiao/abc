82913b209e18071a94e9d2c55f78e9aa9af41e76
guoyangqiao <Gu0yangqiao>
2019-04-10 21:12:47 +0800

    autocommit

--- js/server.js
+++ js/server.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('/lifecycle/scan', (req, response) => {
-    response.set('Content-Type', 'application/json').send({qrCode: !bot.logonoff() ? botContext.qrCode : null});
+    response.set('Content-Type', 'application/json').send({qrCode: !bot.logonoff() ? botContext.qrCode : ''});
 });
 
 let logFilePath = path.resolve(`./send_history.log`);
