9e2436bc94dbd8b2397f0682bdd0244db45e1a50
guoyangqiao <Gu0yangqiao>
2019-04-10 12:26:29 +0800

    autocommit

--- js/wechaty.js
+++ js/wechaty.js
@@ -4,7 +4,7 @@ const botContext = {qrCode: ''};
 
 function onScan(qrcode, status) {
     let qrCode = ['http://api.qrserver.com/v1/create-qr-code/?data=', encodeURIComponent(qrcode),].join('');
-    console.log("服务启动结束");
+    console.log("服务启动结束, 在浏览器中访问地址 localhost:3000");
     botContext.qrCode = qrCode;
 }
 
